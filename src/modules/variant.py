1754aebf28eeb948d82ccda51ad95432b57e1d30
SEED0228 <naokitane6828@gmail.com>
2022-06-29 20:40:43 +0900

    シフトの重複が起こり得ないような仕様に変更

--- src/modules/variant.py
+++ src/modules/variant.py
@@ -4,19 +4,15 @@ from itertools import groupby
 
 class Variant(object):
     # 初期化
-    def __init__(self, all_shifts, parameters, list=None):
-        self.all_shifts = all_shifts
-        self.length = len(all_shifts)
+    def __init__(self, shift_dict, parameters, list=None):
+        self.shift_dict = shift_dict
+        self.length = len(shift_dict)
         self.parameters = parameters
         if list == None:
             self.make_sample()
         else:
             self.list = list
         self.selected_shifts = []
-        for id, x in enumerate(self.list):
-            if x == 1:
-                self.selected_shifts.append(self.all_shifts[id])
-    pass
 
     # ランダムなデータを生成
     def make_sample(self):
@@ -25,6 +21,14 @@ class Variant(object):
             sample_list.append(random.randint(0, 60))
         self.list = tuple(sample_list)
     
+    # 選択されたシフトを作成
+    def create_selected_shifts(self):
+        self.selected_shifts = []
+        copy_dict_values = list(self.shift_dict.values())
+        for id, x in enumerate(self.list):
+            shifts = copy_dict_values[id]
+            if x < len(shifts):
+                self.selected_shifts.append(shifts[x])
     # 選択されたシフトを取得
     def get_selected_shifts(self):
         selected_shifts = self.selected_shifts.copy()
@@ -37,37 +41,17 @@ class Variant(object):
     # 合計の給料を取得
     def sum_total_salary(self):
         return sum(x['salary'] for x in self.selected_shifts)
-    
-    # 同じ日に複数のシフトが入っていないかのチェック
-    def count_duplicated_shifts(self):
-        cnt = 0
-        selected_shifts = self.selected_shifts.copy()
-        for _, shifts in groupby(selected_shifts, key=lambda x: x['start_time'].strftime('%Y-%m-%d')):
-            l = len(list(shifts))
-            if l > 1:
-                cnt += l
-        return cnt
-    
-    # 出勤する日数の出力
-    def count_days(self):
-        selected_shifts = self.selected_shifts.copy()
-        return len(list(groupby(selected_shifts, key=lambda x: x['start_time'].strftime('%Y-%m-%d'))))
 
     # 遺伝的アルゴリズムにおける評価を行う
     def eval(self):
         # 希望した給料との差異
         salary_diff = abs(self.sum_total_salary() - self.parameters['expected_salary']) / max(self.sum_total_salary(), self.parameters['expected_salary'])
-        # 日付が重複したシフトの割合
-        duplicated_shift_count = self.count_duplicated_shifts() / self.length
         # 週に何日で出たいかからこの期間で大体何日入りたいかを算出
-        from_date = dt.strptime(self.parameters['period']['from'], '%Y-%m-%d')
-        to_date = dt.strptime(self.parameters['period']['to'], '%Y-%m-%d')
-        days = (to_date - from_date).days + 1
-        count_days = self.count_days()
-        expected_days = round(self.parameters['expected_day_per_week'] / 7 * days)
-        print(f"{expected_days}, {count_days}")
+        count_days = len(self.selected_shifts)
+        expected_days = round(self.parameters['expected_day_per_week'] / 7 * self.parameters['days'])
+        print(f"{expected_days}, {count_days}, {salary_diff}")
         # 希望勤務日数との差異
         days_diff = abs(expected_days - count_days) / max(expected_days, count_days)
-        return salary_diff, duplicated_shift_count, days_diff,
+        return salary_diff, days_diff,
         
 
