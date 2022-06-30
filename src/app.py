a288846fcec780cbe9a213574a44be7fc35022ba
SEED0228 <naokitane6828@gmail.com>
2022-06-30 11:47:40 +0900

    フロント側でのCORSエラーの対処

--- src/app.py
+++ src/app.py
@@ -7,5 +7,11 @@ app.register_blueprint(test.app, url_prefix='/api/v1/test')
 app.register_blueprint(users.app, url_prefix='/api/v1/users')
 app.register_blueprint(shifts.app, url_prefix='/api/v1/shifts')
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    return response
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
